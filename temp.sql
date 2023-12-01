4cec239933ee30b8b166d7e2f365ccac1c23bb06
ox-i <oxi1224@o2.pl>
2023-12-01 21:23:24 +0100

    delete random files

--- temp.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM "CaseDrop"
-WHERE ctid NOT IN (
-   SELECT min(ctid)                    -- ctid is NOT NULL by definition
-   FROM   "CaseDrop"
-   GROUP  BY "CaseDrop"."weaponName", "CaseDrop"."skinName", "CaseDrop"."skinQuality");  -- list columns defining duplicates
